e5e863102729725acaf4baa8788f4588b750b0bf
MerhanMustafov <76586375+MerhanMustafov@users.noreply.github.com>
2021-01-15 14:42:27 +0200

    Update 09. Easter Cozonacs.py

--- Python_Fundamental/01 Basic Syntax, Conditional Statements and Loops - Exercise/09. Easter Cozonacs.py
+++ Python_Fundamental/01 Basic Syntax, Conditional Statements and Loops - Exercise/09. Easter Cozonacs.py
@@ -35,4 +35,4 @@ while budget > total_price_per_cozunac:
     budget -= total_price_per_cozunac
     if cozunacs_counter % 3 == 0:
         colored_eggs -= (cozunacs_counter - 2)
-print(f"You made {cozunacs_counter} cozonacs! Now you have {colored_eggs} eggs and {budget}BGN left.")
+print(f"You made {cozunacs_counter} cozonacs! Now you have {colored_eggs} eggs and {budget}BGN left.)
