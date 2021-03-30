cd4298a743eb0e139f65290bc59dc72b6d4af3b3
MerhanMustafov <m.o.mustafov@gmail.com>
2021-03-30 19:21:35 +0300

    add one more solutione but different to 02 emoji detector from 05 dinal exam

--- Python_Fundamental/EXAMS/Final_Exams/05. Programming Fundamentals Final Exam/02. Emoji Detector.py
+++ Python_Fundamental/EXAMS/Final_Exams/05. Programming Fundamentals Final Exam/02. Emoji Detector.py
@@ -23,3 +23,24 @@ for num in range(len(valid_emojis)):
     if sum >= threshold:
         print(''.join(valid_emojis[n]))
 
+# import re
+#
+# text = input()
+#
+# pattern = r"(::|\*\*)(?P<emoji>[A-Z][a-z]{2,})(\1)"
+# valid_emojis = [match.group() for match in re.finditer(pattern, text)]
+# cool_threshold = 1
+# for i in text:
+#     if i.isdigit():
+#         i = int(i)
+#         cool_threshold *= i
+# print(f"Cool threshold: {cool_threshold}")
+# print(f"{len(valid_emojis)} emojis found in the text. The cool ones are:")
+#
+# for el in valid_emojis:
+#     sum_of_letters = 0
+#     for char in el:
+#         if char.isalpha():
+#             sum_of_letters += ord(char)
+#     if sum_of_letters >= cool_threshold:
+#         print(el)
