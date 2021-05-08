c28c0ccb74f2a8e9b25892acfb292035eea43f89
MerhanMustafov <m.o.mustafov@gmail.com>
2021-05-08 10:41:48 +0300

    made changes to folders name and add Python advanced first lecture

---
+++ 02 Python_Fundamental/01 Basic Syntax, Conditional Statements and Loops - Exercise/09. Easter Cozonacs.py
@@ -0,0 +1,20 @@
+budget = float(input())
+price_1kg_flour = float(input())
+
+price_egg = price_1kg_flour * 0.75
+pricee_1l_milk = price_1kg_flour * 1.25
+price_0_250l_milk = pricee_1l_milk/4
+
+total_price_per_cozunac = price_1kg_flour + price_egg + price_0_250l_milk
+
+cozunacs_counter = 0
+colored_eggs = 0
+
+while budget > total_price_per_cozunac:
+    cozunacs_counter += 1
+    colored_eggs += 3
+    budget -= total_price_per_cozunac
+    if cozunacs_counter % 3 == 0:
+        colored_eggs -= (cozunacs_counter - 2)
+print(f'You made {cozunacs_counter} cozonacs! Now you have {colored_eggs} eggs and {budget:.2f}BGN left.')
+
