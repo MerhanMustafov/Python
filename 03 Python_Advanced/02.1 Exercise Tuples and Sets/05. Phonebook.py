2e55325fa13b3cb6e9b26987412f0093054bf2a2
MerhanMustafov <m.o.mustafov@gmail.com>
2021-05-13 16:02:35 +0300

    add solution WITH functions to 5th ex from 02.1

--- 03 Python_Advanced/02.1 Exercise Tuples and Sets/05. Phonebook.py
+++ 03 Python_Advanced/02.1 Exercise Tuples and Sets/05. Phonebook.py
@@ -1,20 +1,21 @@
-command = input()
-phone_book = {}
-while not command.isdigit():
-    command = command.split("-")
-    phone_book[command[0]] = command[1]
-
+def fill_phone_book():
     command = input()
-n = int(command)
-for _ in range(n):
-    name = input()
+    phone_book = {}
+    while not command.isdigit():
+        command = command.split("-")
+        phone_book[command[0]] = command[1]
+
+        command = input()
+    return phone_book, int(command)
+def search_contact(contact_name, phone_book):
     if name in phone_book:
         print(f"{name} -> {phone_book[name]}")
     else:
         print(f"Contact {name} does not exist.")
-# for k, v in phone_book.items():
-#     if k in phone_book:
-#         print(f"{k} -> {v}")
-#     else:
-#         print(f"Contact {k} does not exist.")
+
+phone_book, iterations_count  = fill_phone_book()
+for _ in range(iterations_count):
+    name = input()
+    search_contact(name, phone_book)
+
 
