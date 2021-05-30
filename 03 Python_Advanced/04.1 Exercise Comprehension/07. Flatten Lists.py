472bd13bd94a72b1651ab0ca1b2467a70fbf978d
MerhanMustafov <m.o.mustafov@gmail.com>
2021-05-30 19:05:01 +0300

    add solution to 7th ex from 04.1 up to 75/100 on Judge TODO

---
+++ 03 Python_Advanced/04.1 Exercise Comprehension/07. Flatten Lists.py
@@ -0,0 +1,15 @@
+# numbers = input().split("|")
+# # print(numbers)
+# # [[print('  '.join(x), end="") for x in n if x.isdigit()]for n in reversed(numbers)]
+# new_num = []
+# for n in reversed(numbers):
+#     for x in n:
+#         if x.isnumeric():
+#             new_num.append(x)
+#             # print(x, end=" ")
+# print(*new_num, sep=" ")
+
+regular_list = input().split("|")
+flat_list = [item for sublist in reversed(regular_list) for item in sublist if item.isdigit()]
+# print('Original list', regular_list)
+print(' '.join(flat_list))
