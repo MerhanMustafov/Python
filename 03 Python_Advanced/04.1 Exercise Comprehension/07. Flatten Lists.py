8c6af79b2b9329dee085e8f006f71fcf2628f6e3
MerhanMustafov <m.o.mustafov@gmail.com>
2021-06-03 16:09:41 +0300

    add 100/100 solution to 7th ex from 04.1

--- 03 Python_Advanced/04.1 Exercise Comprehension/07. Flatten Lists.py
+++ 03 Python_Advanced/04.1 Exercise Comprehension/07. Flatten Lists.py
@@ -1,21 +1,12 @@
-# numbers = input().split("|")
-# # print(numbers)
-# # [[print('  '.join(x), end="") for x in n if x.isdigit()]for n in reversed(numbers)]
-# new_num = []
-# for n in reversed(numbers):
-#     for x in n:
-#         if x.isnumeric():
-#             new_num.append(x)
-#             # print(x, end=" ")
-# print(*new_num, sep=" ")
-
-# regular_list = input().split("|")
-# flat_list = [item for sublist in reversed(regular_list)for item in sublist if item.isdigit()]
-# # print('Original list', regular_list)
-# print(' '.join(flat_list))
-
-result = [' '.join(list_as_string.split())for list_as_string in input().split("|")[::-1]]
-print(*result, sep=" ")
+numbers = input().split("|")
+new_num = []
+for n in reversed(numbers):
+    for x in n.split():
+        if x.isnumeric(): #or if x != " ":
+            new_num.append(x)
+print(*new_num, sep=" ")
+# result = (' '.join([i for sub in input().split("|")[::-1]for i in sub.split() if i != " "]))
+# print(result)
 
 
 
