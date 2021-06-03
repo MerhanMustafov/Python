6cbdbc68c6183ce007d15beb8cd68e5c1b85c79c
MerhanMustafov <m.o.mustafov@gmail.com>
2021-06-03 17:12:51 +0300

    add 2nd and 3rd ex from 05.1

---
+++ 03 Python_Advanced/05.1 Exercise Functions Advanced/02. Sort.py
@@ -0,0 +1,2 @@
+numbers_list = [int(el) for el in input().split()]
+print(sorted(numbers_list, key= lambda num: num))
---
+++ 03 Python_Advanced/05.1 Exercise Functions Advanced/03. Min Max and Sum.py
@@ -0,0 +1,2 @@
+numbers = [int(el) for el in input().split()]
+print(f"The minimum number is {min(numbers)}\nThe maximum number is {max(numbers)}\nThe sum number is: {sum(numbers)}")
