ca4b10ebc0eef57e8587eab13853109f00995f6d
MerhanMustafov <m.o.mustafov@gmail.com>
2021-07-01 22:11:42 +0300

    add 5th ex from 02

---
+++ 04 Python_OOP/02 Exercise First Steps in OOP/05. Flower.py
@@ -0,0 +1,24 @@
+class Flower:
+    def __init__( self, name, water_requirenments, is_happy = False):
+        self.name = name
+        self.water_requirenments = water_requirenments
+        self.is_happy = is_happy
+    def water(self, quantity):
+        if quantity >= self.water_requirenments:
+            self.is_happy = True
+        else:
+            self.is_happy = False
+    def status(self):
+        if self.is_happy == True:
+            return f"{self.name} is happy" 
+        elif self.is_happy == False:
+            return f"{self.name} is not happy" 
+
+
+flower = Flower("Lilly", 100)
+flower.water(50)
+print(flower.status())
+flower.water(60)
+print(flower.status())
+flower.water(100)
+print(flower.status())
