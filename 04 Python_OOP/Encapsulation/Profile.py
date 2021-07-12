63c32c1846de82a676030cc0a94efaa7228077a6
MerhanMustafov <m.o.mustafov@gmail.com>
2021-07-12 13:24:06 +0300

    oop commits

--- 04 Python_OOP/Encapsulation/Profile.py
+++ 04 Python_OOP/Encapsulation/Profile.py
@@ -1,7 +1,9 @@
+def password(password):
+    return ["*" for x in range(len(password))]
 class Profile:
     def __init__(self, username, password):
         self.username = username
-        self.password = password
+        self.password = password  
     @property
     def username(self):
         return self.__username
@@ -11,8 +13,27 @@ class Profile:
         if not 5 <= len(user) <= 15:
             raise ValueError("The username must be between 5 and 15 characters.")
         self.__username = user
-    # def __str__(self):
-    #     return "You have a profile with username: "{self.username}" and password: "{'*' len(self.password)}".
+    @property
+    def password(self):
+        return self.__password
+    @password.setter
+    def password(self, password):
+        digit = [x for x in password if x.isdigit()]
+        upper_case = [x for x in password if x.isupper()]
+        if not (len(password) >= 8 and digit and upper_case):
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+        self.__password = password
+        
 
-profile_with_invalid_username = Profile('Too_long_userasdasda', 'Any') 
-print(profile_with_invalid_username.username)
+    def __str__(self):
+        p = "".join(password(self.__password))
+        return f'You have a profile with username: "{self.username}" and password: {p}'
+
+
+
+# profile_with_invalid_username = Profile('Too_long_userasdasda', 'Any') 
+# print(profile_with_invalid_username.username)
+correct_profile = Profile("Username", "Passw0rd")
+print(correct_profile)
+
+# profile_with_invalid_password = Profile('My_username', 'My-password')
