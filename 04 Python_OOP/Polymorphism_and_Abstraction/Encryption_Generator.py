3f27c4bfb89cbb533679f2c0f689c070498ff8d6
MerhanMustafov <m.o.mustafov@gmail.com>
2021-07-19 13:35:39 +0300

    oop commits

---
+++ 04 Python_OOP/Polymorphism_and_Abstraction/Encryption_Generator.py
@@ -0,0 +1,25 @@
+class EncryptionGenerator:
+    START = 32
+    END = 127 
+    def __init__(self, text: str):
+        self.text = text
+    def __add__(self, other):
+        if not isinstance(other, (float, int)):
+            raise ValueError("You must add a number.")
+        result = ""
+        for el in self.text:
+            num = ord(el) + other
+            while num < 32:
+                num += 95
+            while num > 126:
+                num -= 95
+            result += chr(num)
+            
+        return result
+some_text = EncryptionGenerator('I Love Python!')
+print(some_text + 1)
+print(some_text + (-1))
+example = EncryptionGenerator('Super-Secret Message')
+print(example + 20)
+print(example + (-52))
+
---
+++ 04 Python_OOP/Polymorphism_and_Abstraction/Image_Area.py
@@ -0,0 +1,4 @@
+string = 'I Love Python!'
+for x in string:
+    x = ord(x) + 1 
+    print(chr(x), end=" ")
