2de06a150f294a161642bc0939a8dee8a24392f5
MerhanMustafov <m.o.mustafov@gmail.com>
2021-08-04 20:18:20 +0300

    oop commits

--- 04 Python_OOP/Inheritance _Exercise/Person/project/person.py
+++ 04 Python_OOP/Inheritance _Exercise/Person/project/person.py
@@ -2,3 +2,25 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
